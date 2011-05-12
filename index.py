5c4c020328c696f066bc41009a1f0ad1db764802
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-05-12 19:53:21 +0100

    Re-enabled index.py Improved formatting to make html look pretty

--- index.py
+++ index.py
@@ -10,26 +10,46 @@ cgitb.enable()
 RT = rtorrent.rtorrent(5000)
 torrentList = RT.getTorrentList()
 
-torrent_html = "<table id='torrent_list'><tr><td class='heading'>Name</td><td class='heading'>Size</td>"
-torrent_html += "<td class='heading'>Ratio</td><td class='heading'>Upload speed</td>"
-torrent_html += "<td class='heading'>Download speed</td></tr>"
+torrent_html = """
+            <table id='torrent_list'>
+                <tr>
+                    <td class='heading'>Name</td>
+                    <td class='heading'>Size</td>
+                    <td class='heading'>Ratio</td>
+                    <td class='heading'>Upload speed</td>
+                    <td class='heading'>Download speed</td>
+                </tr>
+            """
 div_colour_array = ["blue", "green"]
 for t_id, t_name in torrentList.iteritems():
     colour = div_colour_array.pop(0)
     div_colour_array += [colour]
     if RT.conn.d.get_peers_connected(t_id) > 0:
         colour = "red"
-    torrent_html += "<tr onmouseover='select_torrent(this);' onmouseout='deselect_torrent(this);' onclick='view_torrent(this);' class='torrent-div %s' id='torrent_id_%s'>" % (colour, t_id)
-    torrent_html += "<td>%s</td><td>%i MiB</td>" % (t_name, int(float(RT.getSizeBytes(t_id))/1024/1024))
-    torrent_html += "<td title='%.02f up / %.02f down'>%.02f</td>" % (float(RT.getUploadBytes(t_id))/1024/1024, float(RT.getDownloadBytes(t_id))/1024/1024, float(RT.getRatio(t_id))/1000 )
-    torrent_html += "<td>%s KB/s</td><td>%s KB/s</td>" % (int(float(RT.getUploadSpeed(t_id))/1024), int(float(RT.getDownloadSpeed(t_id))/1024))
-    torrent_html += "</tr>"
-torrent_html += "</table>"
-print """Content-Type:text/html\n\n
-Hi!
-"""
-if False:
-    print """Content-Type: text/html\n\n
+    torrent_html += """
+                <tr onmouseover='select_torrent(this);' 
+                    onmouseout='deselect_torrent(this);' 
+                    onclick='view_torrent(this);' 
+                    class='torrent-div %(colour)s' 
+                    id='torrent_id_%(t_id)s'>
+                    <td>%(t_name)s</td>
+                    <td>%(t_size)s MiB</td>
+                    <td title='%(t_uploaded).02f up / %(t_downloaded).02f down'>%(t_ratio).02f</td>
+                    <td>%(t_uprate)s KB/s</td><td>%(t_downrate)s KB/s</td>
+                </tr>""" % {
+                            "colour" : colour,
+                            "t_id" : t_id,
+                            "t_name" : t_name,
+                            "t_size" : int(float(RT.getSizeBytes(t_id))/1024/1024),
+                            "t_uploaded" : float(RT.getUploadBytes(t_id))/1024/1024,
+                            "t_downloaded" : float(RT.getDownloadBytes(t_id))/1024/1024,
+                            "t_ratio" : float(RT.getRatio(t_id))/1000,
+                            "t_uprate" : int(float(RT.getUploadSpeed(t_id))/1024),
+                            "t_downrate" : int(float(RT.getDownloadSpeed(t_id))/1024),
+                        }
+torrent_html += "           </table>"
+
+print """Content-Type: text/html\n\n
     <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
     <html>
         <head>
