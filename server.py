89b7c82852f102bf42980e0d3760ce20b4e149d1
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-05-14 15:28:59 +0100

    Altered detail.js to redirect to web/index.py and altered server to (possibly) server cgi properly

--- server.py
+++ server.py
@@ -4,6 +4,6 @@ import BaseHTTPServer
 import CGIHTTPServer
 
 class Handler(CGIHTTPServer.CGIHTTPRequestHandler):
-	cgi_directories = ["web"]
+	cgi_directories = ["/web"]
 server = BaseHTTPServer.HTTPServer(("mountainpenguin.org.uk",8000), Handler)
 server.serve_forever()
