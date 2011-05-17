936ffac2b4230f021f7ba3c1ddb0d44a70546ba6
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-05-17 23:32:41 +0100

    Try some javascript children navigation

--- static/javascript/file.js
+++ static/javascript/file.js
@@ -1,4 +1,27 @@
 window.onload = function () {
     var files_div = document.getElementById("files_list");
-    fileChildren = files_div.children
+    docroot = files_div.children[0]
+    process_children(docroot);
+    
+}
+
+
+function process_children(elem) {
+    if (elem.hasChildren) {
+        children = elem.children
+        for (index=0; index<children.length; index++) {
+            child = children[child]
+            // child.style.display = "none";
+            if (child.className == "directory") {
+                child.addEventListener("click", function () {
+                    show_contents(child);
+                }
+                process_children(child);
+            }
+        }
+    }
+}
+
+function show_contents(elem) {
+    alert("Show Files!");
 }
