56521f861b9510c45d8bf64b50716f8488647540
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-05-18 00:02:55 +0100

    Minor tweak

--- server.py
+++ server.py
@@ -158,16 +158,16 @@ class mainHandler:
                         <div class="directory" onclick="show_contents(this);"><span class="directory_name">directory 1</span><span class="directory_size">200 KB</span>
                             <div class="directory" onclick="show_contents(this);" style="display:none;"><span class="directory_name">directory 1.1</span><span class="directory_size">100 KB</span>
                                 <div class="document">
-                                    <span class="document_name" style="display:none;">document 1.1-1</span><span class="document_size">10 KB</span>
+                                    <span class="document_name">document 1.1-1</span><span class="document_size">10 KB</span>
                                 </div>
                                 <div class="document">
-                                    <span class="document_name" style="display:none;">document 1.1-2</span><span class="document_size">10 KB</span>
+                                    <span class="document_name">document 1.1-2</span><span class="document_size">10 KB</span>
                                 </div>
                                 <div class="document">
-                                    <span class="document_name" style="display:none;">document 1.1-3</span><span class="document_size">10 KB</span>
+                                    <span class="document_name">document 1.1-3</span><span class="document_size">10 KB</span>
                                 </div>
                                 <div class="document">
-                                    <span class="document_name" style="display:none;">document 1.1-4</span><span class="document_size">10 KB</span>
+                                    <span class="document_name">document 1.1-4</span><span class="document_size">10 KB</span>
                                 </div>
                             </div>
                             <div class="document" style="display:none;">
