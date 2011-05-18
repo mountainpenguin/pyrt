b0d1c8f3fa3c035d6f7536543b9ac58b15170eac
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-05-18 02:10:17 +0100

    Debugging tagName

--- static/javascript/file.js
+++ static/javascript/file.js
@@ -1,6 +1,7 @@
 function show_contents(elem) {
     children = elem.children;
     for (i=0; i<children.length; i++) {
+        alert(children[i].tagName);
         if (children[i].tagName == "span") {
             children[i].style.display="inline";
         } else {
