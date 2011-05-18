26b0f2c8d9292b87aef76e07b838dc9b41acf545
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-05-18 22:40:07 +0100

    Removed trailing semicolons

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     $(".torrent-div.rcstart").contextMenu("right_click_start", {
         bindings : {},
         menuStyle : {
-            min-width : "10em";
+            min-width : "10em"
         }
     });
     $(".torrent-div.rcpause").contextMenu("right_click_pause", {
         bindings : {},
         menuStyle : {
-            min-width : "10em";
+            min-width : "10em"
         }
     });
 });
