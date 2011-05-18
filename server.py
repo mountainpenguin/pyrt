eb7bed509375b861e5c6b835d5c0cd13a1d342fb
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-05-18 02:58:04 +0100

    Fixed bug; various cosmetic changes

--- server.py
+++ server.py
@@ -157,28 +157,30 @@ class mainHandler:
                         
                         <div class="directory">
                             <img alt="Show Contents" title="Show Contents" onclick="show_contents(this.parentNode);" src="/images/folder.png" class="file_img">
-                            <span class="directory_name">directory 1</span><span class="directory_size">200 KB</span>
+                            <span class="directory_name">directory 1</span> <span class="directory_size">200 KB</span>
                             <div class="directory" style="display:none;">
-                                <img alt="Show Contents" title="Show Contents" onclick="show_contents(this.parentNode);" src="/images/folder.png" class="file_img"
+                                <img alt="Show Contents" title="Show Contents" onclick="show_contents(this.parentNode);" src="/images/folder.png" class="file_img">
+                                <span class="directory_name">directory 1.1</span> <span class="directory_size">100 KB</span>
+                                <div class="document" style="display:none;">
                                     <img alt="Document" src="/images/document.png" class="file_img">
-                                    <span class="document_name">document 1.1-1</span><span class="document_size">10 KB</span>
+                                    <span class="document_name">document 1.1-1</span> <span class="document_size">10 KB</span>
                                 </div>
                                 <div class="document" style="display:none;">
                                     <img alt="Document" src="/images/document.png" class="file_img">
-                                    <span class="document_name">document 1.1-2</span><span class="document_size">10 KB</span>
+                                    <span class="document_name">document 1.1-2</span> <span class="document_size">10 KB</span>
                                 </div>
                                 <div class="document" style="display:none;">
                                     <img alt="Document" src="/images/document.png" class="file_img">
-                                    <span class="document_name">document 1.1-3</span><span class="document_size">10 KB</span>
+                                    <span class="document_name">document 1.1-3</span> <span class="document_size">10 KB</span>
                                 </div>
                                 <div class="document" style="display:none;">
                                     <img alt="Document" src="/images/document.png" class="file_img">
-                                    <span class="document_name">document 1.1-4</span><span class="document_size">10 KB</span>
+                                    <span class="document_name">document 1.1-4</span> <span class="document_size">10 KB</span>
                                 </div>
                             </div>
                             <div class="document" style="display:none;">
                                 <img alt="Document" src="/images/document.png" class="file_img">
-                                <span class="document_name">document 1-1</span><span class="document_size">10 KB</span>
+                                <span class="document_name">document 1-1</span> <span class="document_size">10 KB</span>
                             </div>
                         </div>
                         
