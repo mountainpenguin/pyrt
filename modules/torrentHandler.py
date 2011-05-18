5426823345986d996ec143184b9468f54d746028
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-05-18 15:19:16 +0100

    First test on parsing an 'infinite' tree

--- modules/torrentHandler.py
+++ modules/torrentHandler.py
@@ -96,16 +96,16 @@ class Handler:
             random_id = "".join([random.choice(string.letters + string.digits) for i in range(10)])
             files_dict[random_id] = file
             if file.base_path == rtorrent_root:
-                folder["."] = {"_files" : [random_id]}
+                folder["."] = {"___files" : [random_id]}
                 break
             else:
                 if os.path.basename(file.base_path) not in folder.keys():
                     #create folder entry
                     folder[os.path.basename(file.base_path)] = {
-                        "_files" : [],
-                        "_size" : 0,
-                        "_priority" : [file.priority],
-                        "_completion" : 0,
+                        "___files" : [],
+                        "___size" : 0,
+                        "___priority" : [file.priority],
+                        "___completion" : 0,
                     }
                     
                 for index in range(len(file.path_components)):
@@ -118,17 +118,17 @@ class Handler:
                         branch = folder[base]
                         rec_index = 0
                         while rec_index < index:
-                            branch["_size"] += file.size
-                            if file.priority not in branch["_priority"]:
-                                branch["_priority"] += [file.priority]
-                            branch["_completion"] = int((float(branch["_completion"]) + file.percentage_complete) / 2)
+                            branch["___size"] += file.size
+                            if file.priority not in branch["___priority"]:
+                                branch["___priority"] += [file.priority]
+                            branch["___completion"] = int((float(branch["___completion"]) + file.percentage_complete) / 2)
                             branch = branch[file.path_components[rec_index]]
                             rec_index += 1
-                        branch["_files"] += [random_id]
-                        branch["_size"] += file.size
-                        if file.priority not in branch["_priority"]:
-                            branch["_priority"] += [file.priority]
-                        branch["_completion"] = int((float(branch["_completion"]) + file.percentage_complete) / 2)
+                        branch["___files"] += [random_id]
+                        branch["___size"] += file.size
+                        if file.priority not in branch["___priority"]:
+                            branch["___priority"] += [file.priority]
+                        branch["___completion"] = int((float(branch["___completion"]) + file.percentage_complete) / 2)
                     else:
                         #it's a dir
                         #count index up
@@ -138,72 +138,88 @@ class Handler:
                             if file.path_components[rec_index] not in branch.keys():
                                 #create folder entry
                                 branch[file.path_components[rec_index]] = {
-                                    "_files" : [],
-                                    "_size" : 0,
-                                    "_priority" : [],
-                                    "_completion" : 0,
+                                    "___files" : [],
+                                    "___size" : 0,
+                                    "___priority" : [],
+                                    "___completion" : 0,
                                 }
                             branch = branch[file.path_components[rec_index]]
                             rec_index += 1
                             
         return (folder, files_dict)
-                        
-                        
         
-    #def getFileStructure(self, files, rtorrent_root):
-    #    """
-    #        Takes a list of <files> (as outputted by rtorrent.getFiles) and converts it into a file structure
-    #        Returns a tuple of two dictionaries
-    #            ( files , files_dict )
-    #        Files contains the file hierachy (two levels maximum) with file_id strings
-    #        Files_dict contains the original rtorrent.File instances indexed by their file_id strings
-    #    """
-    #    folder = {}
-    #    files_dict = {}
-    #    priority_lookup = {"high" : 2, "normal" : 1, "off" : 0}
-    #    for file in files:
-    #        random_id = "".join([random.choice(string.letters + string.digits) for i in range(10)])
-    #        files_dict[random_id] = file
-    #        if file.base_path == rtorrent_root:
-    #            folder["."] = {"_files" : [random_id]}
-    #        else:
-    #            if len(file.path_components) == 1:
-    #                if os.path.basename(file.base_path) not in folder.keys():
-    #                    folder[os.path.basename(file.base_path)] = {"_files" : [random_id], "_size" : file.size, "_priority" : [file.priority], "_completion" : file.percentage_complete}
-    #                else:
-    #                    folder[os.path.basename(file.base_path)]["_files"] += [random_id]
-    #                    folder[os.path.basename(file.base_path)]["_size"] += file.size
-    #                    if file.priority not in folder[os.path.basename(file.base_path)]["_priority"]:
-    #                        folder[os.path.basename(file.base_path)]["_priority"] += [file.priority]
-    #                    prev = folder[os.path.basename(file.base_path)]["_completion"]
-    #                    new = (prev + file.percentage_complete) / 2
-    #                    folder[os.path.basename(file.base_path)]["_completion"] = new
-    #            else:
-    #                if os.path.basename(file.base_path) not in folder.keys():
-    #                    folder[os.path.basename(file.base_path)] = {file.path_components[0] : {"_files" : [random_id], "_size" : file.size, "_priority" : [file.priority], "_completion" : file.percentage_complete}, "_files" : [], "_size" : file.size, "_priority" : [file.priority], "_completion" : file.percentage_complete}
-    #                else:
-    #                    if file.path_components[0] not in folder[os.path.basename(file.base_path)].keys():
-    #                        folder[os.path.basename(file.base_path)][file.path_components[0]] = {"_files" : [random_id], "_size" : file.size, "_priority" : [file.priority], "_completion" : file.percentage_complete}
-    #                    else:
-    #                        folder[os.path.basename(file.base_path)][file.path_components[0]]["_files"] += [random_id]
-    #                        folder[os.path.basename(file.base_path)][file.path_components[0]]["_size"] += file.size
-    #                        if file.priority not in folder[os.path.basename(file.base_path)][file.path_components[0]]["_priority"]:
-    #                            folder[os.path.basename(file.base_path)][file.path_components[0]]["_priority"] += [file.priority]
-    #                        folder[os.path.basename(file.base_path)][file.path_components[0]]["_completion"] = (folder[os.path.basename(file.base_path)][file.path_components[0]]["_completion"] + file.percentage_complete) / 2
-    #    return (folder, files_dict)
+    def fileTreeHTML2(self, fileList, RTROOT):
+        """
+            Takes a list of files as outputted by rtorrent.getFiles and parses it into an html file tree
+            Requires the rtorrent root directory
+            File attributes:
+                abs_path, base_path, path_components, completed_chunks, priority, size, chunks, chunk___size
+        """
+        DOCUMENT_DIV = """
+            <div class="document"%s>
+                <img alt="Document" src="/images/document.png" class="file_img">
+                <span class="document_name">%s</span> 
+                <span class="directory_size">%s</span>
+            </div>
+        """
+        DIRECTORY_DIV = """
+            <div class="directory"%s>
+                <img alt="Show Contents" title="Show Contents" onclick="event.cancelBubble = true; show_contents(this.parentNode);" src="/images/folder.png" class="file_img" style="cursor:pointer;">
+                <span class="directory_name">%s</span>
+                <span class="directory_size">%s</span>
+        """
+        
+        HIDDEN = " style=\"display:none;\""
+        
+        def _getFiles(level):
+            html = ""
+            for file in level["___files"]:
+                html += DOCUMENT_DIV % (HIDDEN, os.path.basename(fileDict[file].abs_path), fileDict[file].size)
+            return html
+            
+        def _getDirs(level):
+            level_keys = []
+            for _key in level.keys():
+                if _key[0:3] != "___":
+                    level_keys += [_key]
+            level_keys.sort()
+            for subDirName in level_keys:
+                subLevel = level[subDirName]
+                html = DIRECTORY_DIV % (HIDDEN, subDirName, subLevel["_size"])
+                html += _getDirs(subLevel)
+                html += "</div>"
+            return html
+                
+        fileStruct, fileDict = self.getFileStructure(fileList, RTROOT)
+        root_keys = fileStruct.keys()
+        root_keys.sort()
+        if root_keys[0] == ".":
+            fileObj = fileDict[fileStruct["."]["___files"][0]]
+            
+            return """
+                <div id="files_list">
+                    %s
+                </div>
+                """ % (DOCUMENT_DIV % ("", os.path.basename(fileObj.abs_path), self.humanSize(fileObj.size)))
+        else:
+            #walk through dictionary
+            #should only ever be one root_key, "." or the base directory
+            html = "<div id=\"files_list\">"
+            root = fileStruct[root_keys[0]]
+            return _getDirs(root)
         
     def fileTreeHTML(self, fileList, RTROOT):
         """
             Takes a list of files as outputted by rtorrent.getFiles and parses it into an html file tree
             Requires the rtorrent root directory
             File attributes:
-                abs_path, base_path, path_components, completed_chunks, priority, size, chunks, chunk_size
+                abs_path, base_path, path_components, completed_chunks, priority, size, chunks, chunk___size
         """
         DOCUMENT_DIV = """
             <div class="document"%s>
                 <img alt="Document" src="/images/document.png" class="file_img">
                 <span class="document_name">%s</span> 
-                <span class="document_size">%s</span>
+                <span class="directory_size">%s</span>
             </div>
         """
         DIRECTORY_DIV = """
@@ -216,13 +232,13 @@ class Handler:
         HIDDEN = " style=\"display:none;\""
         
         fileStruct, fileDict = self.getFileStructure(fileList, RTROOT)
-        #{'.': {'_files': ['96z9DJ2vWR']}}
-        #{'conky': {'_size': 3611, '_priority': ['normal'], '_completion': 0.0, '_files': ['psO5e58GdQ', 'Bxpi3YRclr', 'FV7SfL8V2e', 'Y92Ma4iC2M']}}
-        #{'08 - The Path of Daggers': {'_size': 1343817670, '_priority': ['off'], '_completion': 0.0, '_files': ['odPoI2rbhS', 'NBmOGXd1m2', '8HXWKF9MnA', 'L92axMfMoV', 'rtO44f2aYt', 'PoKyX2bLDL', '7OuK4GWZVq', 'wq4AWZxSUL', 'RdZawIqx6g', '6KIzUPSGiX', 'KxHmeBp1Aj', 'CtxVm4Yxch', 'tuXzAwNfE8', '10eXBJxyGS', '1wQoDUPkaA', 'Z6JZOXtMUj', 'eV7zUZTP3X', 'TvKCOUDbQy', 'WgoIW9WUT1', 'Ahc2pXgagX', 'xl4AIld6Vt', '5wpbRgnwku', 'BHl2dwWNr1', 's0ikEp7ScB', 'oIiJeD7ocn', 'Gv2dVcxpOa', 'ihC6M4c6T9', 'yE1tFJKhb4', 'kYeVUbIHVD', '7EFR7hxqrD', 'NItqUmoD4v', '5mmAQPow0A', 'lGtQlXYMtO']}, '07 - A Crown of Swords': {'_size': 1765362671, '_priority': ['off'], '_completion': 0.0, '_files': ['Wez3cq82vf', 'NObzRDUtdn', 'pJmFAgKRBM', 'TYgBfC7ZtJ', 'BRK1C0zNyo', 'u9gLa9VK1b', '7D87LV1u65', 'BeMUnkoyzk', 'HEftyzn79d', 'BmD712zuUO', '7HQleqYP7s', 'ZSeIt1Mbz1', 'TSi5rF7cuP', 'JOeYO0fS9N', 'yG4W5RJLn2', 'EFOMY2qRaQ', 'oXbigBNqDz', 'Lie9NDYQW2', 'O8zR1Y2Gpn', 'm4CHCRJyEl', 'OLsGadSDfM', 'Kxv9IjbCXz', '5pbr33sziJ', 'gIUVN4QW3C', 'rWGyLQbytg', 'MIMclfNsoi', 'JDvTUsZXKc', 'Qu8Sx7HNmZ', '1JBV36YOPh', 'sMOigZIlOQ', 'AiG0M442QT', 'xAQGaE0uqz', 'hk0xt79hps', 'mWNvteJfBN', 'fq5QADvHmM', 'NDumdsyGRG', 'KrGPV6AoLu', 'LQFVMbMzS0', 'mINS9DG2RY', 'jsNAkSEN0Y', 'dxjNRxhFMF', 'U1mT4LgJ9J', 'Uem2iveASy', '3igPDbfDpF', 'yuWulCZxlX']}, '02 - The Great Hunt': {'_size': 1535105250, '_priority': ['normal'], '_completion': 100.0, '_files': ['m04mBlUfl2', 'qd3Gw3F2jj', 'B28rEVZqpo', 'ZoFYWrBWGI', 'zH5GVT3GOq', 'SlUlCDkg8P', 'Mscdi612ai', 'yBuYuNVdE1', 'c2tELemMYk', 'eG4bH1Cr33', 'jfjgEJz2Rq', 'vwDFAxBnS3', 'QrpEDyoZzc', 'VgGMFwQ0bo', 'oQV48LfsS6', 'Mfpt7IhQdE', 'RYvVGp2ujF', 'TQXIv9X0V1', 'IeVDW1CcbZ', 'oPfIaYStTP', 'GAgD6Kibw1', 'M7a0duJJEg', 'DJdtlJuqXe', 'By3tWiRBcd', '2Fo6BnEFis', 'Jpb54M9NK1', 'PnfOXbZEcu', 'LTkAbfWatf', 'KJsDh9DSJw', 'Ggbi4chIml', 'Or3SVIUzlF', 'NNbsBBCrh7', 'yDpGnXZwRP', 'KhpnFLrOKY', 'lR73aWXCba', 'f9VtRUMus4', 'yjPKtgFAXE', '4GqZfhKrA1', 'jnWFRXgWf2', 'GohODd951O', 'q8CXSrmOyD', 'FSnZDVTsxF', 'eWMNL9MNNq', '9LX1aes4Qw', 'i6jTYbecjD', 'QIkFcfH8gr', '1kMvjnbakG', 'OZYnXqhQpH', 'fTxVbgEFMv', 'QDMoCuaVsb', 'QKHSiVdoxl']}
+        #{'.': {'___files': ['96z9DJ2vWR']}}
+        #{'conky': {'___size': 3611, '___priority': ['normal'], '___completion': 0.0, '___files': ['psO5e58GdQ', 'Bxpi3YRclr', 'FV7SfL8V2e', 'Y92Ma4iC2M']}}
+        #{'08 - The Path of Daggers': {'___size': 1343817670, '___priority': ['off'], '___completion': 0.0, '___files': ['odPoI2rbhS', 'NBmOGXd1m2', '8HXWKF9MnA', 'L92axMfMoV', 'rtO44f2aYt', 'PoKyX2bLDL', '7OuK4GWZVq', 'wq4AWZxSUL', 'RdZawIqx6g', '6KIzUPSGiX', 'KxHmeBp1Aj', 'CtxVm4Yxch', 'tuXzAwNfE8', '10eXBJxyGS', '1wQoDUPkaA', 'Z6JZOXtMUj', 'eV7zUZTP3X', 'TvKCOUDbQy', 'WgoIW9WUT1', 'Ahc2pXgagX', 'xl4AIld6Vt', '5wpbRgnwku', 'BHl2dwWNr1', 's0ikEp7ScB', 'oIiJeD7ocn', 'Gv2dVcxpOa', 'ihC6M4c6T9', 'yE1tFJKhb4', 'kYeVUbIHVD', '7EFR7hxqrD', 'NItqUmoD4v', '5mmAQPow0A', 'lGtQlXYMtO']}, '07 - A Crown of Swords': {'___size': 1765362671, '___priority': ['off'], '___completion': 0.0, '___files': ['Wez3cq82vf', 'NObzRDUtdn', 'pJmFAgKRBM', 'TYgBfC7ZtJ', 'BRK1C0zNyo', 'u9gLa9VK1b', '7D87LV1u65', 'BeMUnkoyzk', 'HEftyzn79d', 'BmD712zuUO', '7HQleqYP7s', 'ZSeIt1Mbz1', 'TSi5rF7cuP', 'JOeYO0fS9N', 'yG4W5RJLn2', 'EFOMY2qRaQ', 'oXbigBNqDz', 'Lie9NDYQW2', 'O8zR1Y2Gpn', 'm4CHCRJyEl', 'OLsGadSDfM', 'Kxv9IjbCXz', '5pbr33sziJ', 'gIUVN4QW3C', 'rWGyLQbytg', 'MIMclfNsoi', 'JDvTUsZXKc', 'Qu8Sx7HNmZ', '1JBV36YOPh', 'sMOigZIlOQ', 'AiG0M442QT', 'xAQGaE0uqz', 'hk0xt79hps', 'mWNvteJfBN', 'fq5QADvHmM', 'NDumdsyGRG', 'KrGPV6AoLu', 'LQFVMbMzS0', 'mINS9DG2RY', 'jsNAkSEN0Y', 'dxjNRxhFMF', 'U1mT4LgJ9J', 'Uem2iveASy', '3igPDbfDpF', 'yuWulCZxlX']}, '02 - The Great Hunt': {'___size': 1535105250, '___priority': ['normal'], '___completion': 100.0, '___files': ['m04mBlUfl2', 'qd3Gw3F2jj', 'B28rEVZqpo', 'ZoFYWrBWGI', 'zH5GVT3GOq', 'SlUlCDkg8P', 'Mscdi612ai', 'yBuYuNVdE1', 'c2tELemMYk', 'eG4bH1Cr33', 'jfjgEJz2Rq', 'vwDFAxBnS3', 'QrpEDyoZzc', 'VgGMFwQ0bo', 'oQV48LfsS6', 'Mfpt7IhQdE', 'RYvVGp2ujF', 'TQXIv9X0V1', 'IeVDW1CcbZ', 'oPfIaYStTP', 'GAgD6Kibw1', 'M7a0duJJEg', 'DJdtlJuqXe', 'By3tWiRBcd', '2Fo6BnEFis', 'Jpb54M9NK1', 'PnfOXbZEcu', 'LTkAbfWatf', 'KJsDh9DSJw', 'Ggbi4chIml', 'Or3SVIUzlF', 'NNbsBBCrh7', 'yDpGnXZwRP', 'KhpnFLrOKY', 'lR73aWXCba', 'f9VtRUMus4', 'yjPKtgFAXE', '4GqZfhKrA1', 'jnWFRXgWf2', 'GohODd951O', 'q8CXSrmOyD', 'FSnZDVTsxF', 'eWMNL9MNNq', '9LX1aes4Qw', 'i6jTYbecjD', 'QIkFcfH8gr', '1kMvjnbakG', 'OZYnXqhQpH', 'fTxVbgEFMv', 'QDMoCuaVsb', 'QKHSiVdoxl']}
         root_keys = fileStruct.keys()
         root_keys.sort()
         if root_keys[0] == ".":
-            fileObj = fileDict[fileStruct["."]["_files"][0]]
+            fileObj = fileDict[fileStruct["."]["___files"][0]]
             
             return """
                 <div id="files_list">
@@ -232,17 +248,17 @@ class Handler:
         else:
             html = "<div id=\"files_list\">"
             for dir in root_keys:
-                html += DIRECTORY_DIV % ("", dir, self.humanSize(fileStruct[dir]["_size"]))
-                files = fileStruct[dir]["_files"]
+                html += DIRECTORY_DIV % ("", dir, self.humanSize(fileStruct[dir]["___size"]))
+                files = fileStruct[dir]["___files"]
                 dirs = []
                 for i in fileStruct[dir].keys():
-                    if i[0] != "_":
+                    if i[0:3] != "___":
                         dirs += [i]
                 dirs.sort()
                 for sub_dir in dirs:
-                    html += DIRECTORY_DIV % (HIDDEN, sub_dir, self.humanSize(fileStruct[dir][sub_dir]["_size"]))
-                    sub_files = fileStruct[dir][sub_dir]["_files"]
-                    for sub_file in sub_files:
+                    html += DIRECTORY_DIV % (HIDDEN, sub_dir, self.humanSize(fileStruct[dir][sub_dir]["___size"]))
+                    sub___files = fileStruct[dir][sub_dir]["___files"]
+                    for sub_file in sub___files:
                         sub_fileObj = fileDict[sub_file]
                         html += DOCUMENT_DIV % (HIDDEN,"/".join(sub_fileObj.path_components[1:]), self.humanSize(sub_fileObj.size))
                     html += "</div>"
@@ -364,7 +380,7 @@ class Handler:
             <table id='torrent_list'>
                 <tr>
                     <td class='heading' id="sortby_name" onclick="window.location='%(name)s';">Name <img alt="Sort By Name" src="../images/sort_%(namesort)s.gif" class="control_button"></td>
-                    <td class='heading' id="sortby_size" onclick="window.location='%(size)s';">Size <img alt="Sort By Size" src="../images/sort_%(sizesort)s.gif" class="control_button"></td>
+                    <td class='heading' id="sortby___size" onclick="window.location='%(size)s';">Size <img alt="Sort By Size" src="../images/sort_%(sizesort)s.gif" class="control_button"></td>
                     <td class='heading' id="sortby_ratio" onclick="window.location='%(ratio)s';">Ratio <img alt="Sort By Ratio" src="../images/sort_%(ratiosort)s.gif" class="control_button"></td>
                     <td class='heading' id="sortby_uprate" onclick="window.location='%(uprate)s';">Upload speed <img alt="Sort By Upload Speed" src="../images/sort_%(upratesort)s.gif" class="control_button"></td>
                     <td class='heading' id="sortby_downrate" onclick="window.location='%(downrate)s';">Download speed <img alt="Sort By Download Speed" src="../images/sort_%(downratesort)s.gif" class="control_button"></td>
@@ -391,7 +407,7 @@ class Handler:
                     class='torrent-div %(colour)s' 
                     id='torrent_id_%(t_id)s'>
                     <td>%(t_name)s</td>
-                    <td>%(t_size)s</td>
+                    <td>%(t___size)s</td>
                     <td title='%(t_uploaded)s up / %(t_downloaded)s down'>%(t_ratio).02f</td>
                     <td>%(t_uprate)s/s</td>
                     <td>%(t_downrate)s/s</td>
@@ -416,7 +432,7 @@ class Handler:
                             "colour" : colour,
                             "t_id" : t.torrent_id,
                             "t_name" : t.name,
-                            "t_size" : self.humanSize(t.size),
+                            "t___size" : self.humanSize(t.size),
                             "t_uploaded" : self.humanSize(t.up_total),
                             "t_downloaded" : self.humanSize(t.down_total),
                             "t_ratio" : float(t.ratio)/1000,
