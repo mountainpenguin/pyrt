ae4a928a4e8100245bbbb41bbf1ee87600211c55
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-05-21 15:44:29 +0100

    Who needs class transition animations anyway?

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -265,7 +265,7 @@ function add_torrentrow(torrent_id, torrent_data) {
             $(newtorrentrow).fadeTo(2000, 1.0, function() {
                 $("#torrent_id_" + torrent_id).effect("pulsate", { times : 1 }, "slow", function () {
                     $("#torrent_id_" + torrent_id).addClass(newcolour);
-                    $("#torrent_id_" + torrent_id).animate({
+                    $("#torrent_id_" + torrent_id).css({
                         backgroundColor : null
                     });
                     loadRClickMenus()
