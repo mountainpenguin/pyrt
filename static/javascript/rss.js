281811bca161663dadf71394318bdabe0d0816d8
mountainpenguin <pinguino.de.montana@googlemail.com>
2011-07-20 21:30:27 +0100

    Add missing js navigation functions

--- static/javascript/rss.js
+++ static/javascript/rss.js
@@ -10,6 +10,14 @@ function deselect_tab(elem) {
     elem.style.backgroundColor = null;
 }
 
+function navigate_tab(elem) {
+    window.location = "?view=" + elem.id.split("tab_")[1];
+}
+
+function navigate_tab_fromRSS(elem) {
+     window.location = "/?view=" + elem.id.split("tab_")[1];
+}
+
 function getFeeds() {
     var feeds = $("#feed_wrapper");
     $.ajax({
