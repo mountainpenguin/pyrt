0b8e4e1638cd82423b386f72b31e48804c7d4712
mountainpenguin <pinguino.de.montana@googlemail.com>
2012-02-02 22:26:09 +0000

    Preliminary test on an options page

--- static/javascript/options.js
+++ static/javascript/options.js
@@ -2,83 +2,21 @@ var rtorrent_display = false;
 var pyrt_display = false;
 
 $(document).ready( function () {
-   //$("#slide-me").hide("slide", { direction : "left" }, 0, function () {
-   //   //$("#tab_pyrt").show("slide", { direction : "left" }, 0);
-   //   //$("#tab_rtorrent").show("slide", { direction : "left" }, 0);
-   //});
-   $("#slide-me").hide()
-   $("#tab_pyrt").show()
-   $("#tab_rtorrent").show()
    $("#tab_main").bind(
       "click",
       function () {
-       window.location = "/?view=main";
+         window.location = "/?view=main";
       }
    );
-   $("#tab_pyrt").bind(
-      "click",
-      function () {
-         if (rtorrent_display) {
-            $("#rtorrent-options-wrapper").hide("slide", { direction : "up" }, 500, function () {
-               $("#pyrt-options-wrapper").show("slide", { direction : "up" }, 500);
-            });
-         } else if (pyrt_display) {
-         } else {
-            $("#pyrt-options-wrapper").show("slide", { direction : "up" }, 500);
-         }
-         pyrt_display = (function () {
-            $("#tab_pyrt").addClass("hover");
-            return true;
-         })();
-         rtorrent_display = (function () {
-            $("#tab_rtorrent").removeClass("hover");
-            return false;
-         })();
+   $("ol.sidebar-list > li").bind(
+      "click", function (e) {
+         $(".selected").each(function () {
+            if (this.id !== "tab_options") {
+               $(this).removeClass("selected");
+            }
+         })
+         $(e.target).addClass("selected");
+         $("#" + e.target.id.split("-tab")[0]).parent().addClass("selected");
       }
-   );
-   $("#tab_rtorrent").bind(
-      "click",
-      function () {
-         if (pyrt_display) {
-            $("#pyrt-options-wrapper").hide("slide", { direction : "up" }, 500, function () {
-               $("#rtorrent-options-wrapper").show("slide", { direction : "up" }, 500);
-            });
-         } else if (rtorrent_display) {
-         } else {
-            $("#rtorrent-options-wrapper").show("slide", { direction : "up" }, 500);
-         }
-         pyrt_display = (function () {
-            $("#tab_pyrt").removeClass("hover");
-            return false;
-         })();
-         rtorrent_display = (function () {
-            $("#tab_rtorrent").addClass("hover");
-            return true;
-         })();
-      }
-   );
-   $("#pyrt-newpw_confirm-input").bind(
-      "change",
-      function () {
-         pw1 = $("#pyrt-newpw-input").val();
-         pw2 = $(this).val();
-         if (pw1 !== pw2) {
-            $("#pyrt-newpw_report-span").html(
-               "X" 
-            ).removeClass("ok").addClass("bad");
-         } else {
-            $("#pyrt-newpw_report-span").html(
-               ":)"
-            ).removeClass("bad").addClass("ok");
-         }
-      }
-   );
+   )
 });
-
-function select_tab(elem) {
-   $(elem).addClass("hover_temp");
-}
-
-function deselect_tab(elem) {
-   $(elem).removeClass("hover_temp");
-}
