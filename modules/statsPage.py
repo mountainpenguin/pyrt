f92785dd71022a00ea5d6b41acf720664f7246f7
mountainpenguin <pinguino.de.montana@googlemail.com>
2012-05-16 01:56:20 +0100

    Licensed under the GPL v3
    Also removed the TODO file (can track using github issues)
    Added files LICENSE and AUTHORS

--- modules/statsPage.py
+++ modules/statsPage.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 
+""" Copyright (C) 2012 mountainpenguin (pinguino.de.montana@googlemail.com)
+    <http://github.com/mountainpenguin/pyrt>
+    
+    This file is part of pyRT.
+
+    pyRT is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pyRT is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pyRT.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from __future__ import print_function
 from modules import config
 from modules import rtorrent
