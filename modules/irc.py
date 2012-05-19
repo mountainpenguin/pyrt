640a2d31180f464e860152d21a72fc7368c963d6
mountainpenguin <pinguino.de.montana@googlemail.com>
2012-05-19 01:41:14 +0100

    Broken commit, attempt at making a UNIX socket file for cross-process communication

--- modules/irc.py
+++ modules/irc.py
@@ -4,6 +4,8 @@ from modules.irclib import irclib
 from modules.irclib import ircbot
 import logging
 import multiprocessing
+import os
+import socket
 
 class _Config(object):
     def __init__(self, **args):
@@ -12,15 +14,19 @@ class _Config(object):
 class _ModularBot(ircbot.SingleServerIRCBot):
     def __init__(self, net, nick, name, *args, **kwargs):
         self.config = _Config(**kwargs)
+        #create unix socket for communication with tornado server
+        self.PID = os.getpid()
+        self._socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        self._socket.bind(".bots/%d.socket" % self.PID)
+
         ircbot.SingleServerIRCBot.__init__(self, net, nick, name)
 
     def on_welcome(self, connection, event):
         connection.join(self.config.channel)
+        self._socket.send("Bot connected\n")
 
     def on_pubmsg(self, connection, event):
-#        self.config.log.debug("IRC: %s said %r in %s", event.source().split("!")[0], event.arguments()[0], event.target())
-#        logging.info("PRIVMSG source: %s target: %s arguments: %r", event.source(), event.target(), event.arguments())
-        pass
+        self._socket.send("PUBMSG: %r\n" % event.arguments()[0])
 
 class Irc(object):
     def __init__(self, log, network="127.0.0.1", channel="#mp-dev", nick="pyrtBot", port=6667):
@@ -31,9 +37,12 @@ class Irc(object):
         self.name = "pyrt"
         self.log = log
 
+    def startbot(self):
+        bot = _ModularBot([(self.network, self.port)], self.nick, self.name, channel=self.channel)
+        bot.start()
+
     def start(self):
         bot = _ModularBot([(self.network, self.port)], self.nick, self.name, channel=self.channel, log=self.log)
-#        p = multiprocessing.Process(target=bot.start)
-#        p.daemon = True
-#        p.start()
-        self.log.info("IRC bot start function executed, but disabling for further dev work")
+        p = multiprocessing.Process(target=self.startbot)
+        p.daemon = True
+        p.start()
