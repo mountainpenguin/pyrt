mountainpenguin/pyrt

commit fdf892b3c503527832d4aedaeb367456d8b33458

Fixed CSS for buttons to float correctly

modules/autohandler.py
@@ -72,11 +72,13 @@ class AutoHandler(object):
                     <div class="settings">
                         <h4>Settings</h4>
                             %(form)s
-                        <button type="submit" class="submit_button" id="submit_%(name)s">
-                            <img src="/images/submit.png" width=20 height=20>
-                            <span>Submit</span>
-                        </button>
-                        %(botbutton)s
+                        <div class="submit_buttons">
+                            <button type="submit" class="submit_button" id="submit_%(name)s">
+                                <img src="/images/submit.png" width=20 height=20>
+                                <span>Submit</span>
+                            </button>
+                            %(botbutton)s
+                        </div>
                     </div>
                     %(filters)s
                 </td>

static/css/auto.css
@@ -140,3 +140,7 @@ thead > tr {
 .is_hidden {
   display: none;
   visibility: hidden; }
+
+.submit_buttons {
+  float: left;
+  clear: both; }

static/scss/auto.scss
@@ -196,3 +196,8 @@ thead {
     display: none;
     visibility: hidden;
 }
+
+.submit_buttons {
+    float: left;
+    clear: both;
+}

commit fa5ce2aa295d3cd85de99d64079ef7ffe31fb000

Fixed bug with adding 'remotes'
Wrapping of submit / start IRC button in extra div in commit
fdf892b3c503527832d4ae caused inputs to not be retrieved correctly,
resulting in no settings being saved for remote handlers

Also added a timeout after starting a bot to allow the bot time to
register and thus be recorded as ON on the /auto page

modules/autohandler.py
@@ -132,9 +132,10 @@ class AutoHandler(object):
            ircobj.start()
         except:
             tb = traceback.format_exc()
-            self.LOG.error("AUTO: error starting IRC bot for handler '%s' - %s", name, tb.strip().split("\n")[-1])
+            tb_line = tb.strip().split("\n")[-1]
+            self.LOG.error("AUTO: error starting IRC bot for handler '%s' - %s", name, tb_line)
             logging.error(tb)
-            return "ERROR/Faulty configuration for handler"
+            return "ERROR/%s" % tb_line
         return json.dumps({
             "request" : "start_bot",
             "error" : None,
@@ -310,7 +311,7 @@ class AutoHandler(object):
             resp = {
                 "request" : "set_source",
                 "error" : None,
-                "response" : "OK",
+                "response" : name,
             }
             return json.dumps(resp)
         else:

static/javascript/auto.js
@@ -121,7 +121,7 @@ $(document).ready( function () {
     });
     $(".submit_button").live("click", function (evt) {
         // get arguments
-        var inputs = $("input", $(this).parent());
+        var inputs = $("input", $(this).parent().parent());
         args = new Object();
         args.tocrypt = [];
         var exit = false;
@@ -168,10 +168,12 @@ function OnMessage (evt) {
           } else if (response.request == "add_filter") {
             refresh_drop_down(response.response);
           } else if (response.request == "start_bot") {
-            refresh_drop_down(response.response);
+            setTimeout(function () { refresh_drop_down(response.response); }, 2000);
           } else if (response.request == "stop_bot") {
             refresh_drop_down(response.response);
           } else if (response.request == "remove_filter") {
+            refresh_drop_down(response.response);
+          } else if (response.request == "set_source") {
             refresh_drop_down(response.response);
           } else {
             console.log("socket message:", evt.data)