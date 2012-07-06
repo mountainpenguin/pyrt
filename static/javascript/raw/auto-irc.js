5b6e5503a5fd4177ead07472a63aec9752f340c7
mountainpenguin <pinguino.de.montana@googlemail.com>
2012-07-06 18:48:36 +0100

    Remove previous input if --- selected
    Also reduce width of filter inputs

--- static/javascript/raw/auto-irc.js
+++ static/javascript/raw/auto-irc.js
@@ -28,12 +28,15 @@ $(document).ready( function () {
     });
     $(".filter_select").live("change", function() {
         if ($(this).val() == "---") {
-            console.log($(this).next());
-            return;
+            if ($(this).prev().hasClass("add_filter")) {
+                return;
+            } else {
+                $(this).prev().remove();
+            }
         } else if ($(this).val() == "AND") {
-            $(this).prev().after($("<input name='add_filter' class='and_filter' type='text' placeholder='Filter' />"));
+            $(this).prev().after($("<input name='add_filter' class='and_filter input_filter' type='text' placeholder='Filter' />"));
         } else if ($(this).val() == "NOT") {
-            $(this).prev().after($("<input name='not_filter' class='not_filter' type='text' placeholder='Negative Filter' />"));
+            $(this).prev().after($("<input name='not_filter' class='not_filter input_filter' type='text' placeholder='Negative Filter' />"));
         }
         console.log("filter_select changed to", $(this).val());
     });
