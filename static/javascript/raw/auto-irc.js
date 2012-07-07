652b6211bfb9ba1b288d28c5b578331f130238ee
mountainpenguin <pinguino.de.montana@googlemail.com>
2012-07-07 12:42:23 +0100

    Actually send filter removal message
    Issue #30

--- static/javascript/raw/auto-irc.js
+++ static/javascript/raw/auto-irc.js
@@ -56,11 +56,10 @@ $(document).ready( function () {
     $(".filter_group").live("click", function () {
         var name = $(this).closest(".remote_setting").attr("id").split("remote_settings_")[1];
         var index = $(".filter_group", $(this).parent()).index($(this));
-        console.log("index is", index);
-        //var c = confirm("Are you sure you want to remove this filter?");
-        //if (c) {
-        //    socket.send("request=remove_filter&name=" + name + "&index=" + index);
-        //}
+        var c = confirm("Are you sure you want to remove this filter?");
+        if (c) {
+            socket.send("request=remove_filter&name=" + name + "&index=" + index);
+        }
     });
     $("#dialog_form").dialog({
         autoOpen: false,
