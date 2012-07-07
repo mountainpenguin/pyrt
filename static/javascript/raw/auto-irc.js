e4950c42284e883045460c444d1363bee9e1faff
mountainpenguin <pinguino.de.montana@googlemail.com>
2012-07-07 12:34:06 +0100

    Debug determination of index
    Issue #30
    
    Also more CSS fixes

--- static/javascript/raw/auto-irc.js
+++ static/javascript/raw/auto-irc.js
@@ -56,10 +56,11 @@ $(document).ready( function () {
     $(".filter_group").live("click", function () {
         var name = $(this).closest(".remote_setting").attr("id").split("remote_settings_")[1];
         var index = $(".filter", $(this).parent()).index($(this));
-        var c = confirm("Are you sure you want to remove this filter?");
-        if (c) {
-            socket.send("request=remove_filter&name=" + name + "&index=" + index);
-        }
+        console.log("index is", index);
+        //var c = confirm("Are you sure you want to remove this filter?");
+        //if (c) {
+        //    socket.send("request=remove_filter&name=" + name + "&index=" + index);
+        //}
     });
     $("#dialog_form").dialog({
         autoOpen: false,
