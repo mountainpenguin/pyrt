d3056c55560194901c044e483bc5fe4fa71d4073
mountainpenguin <pinguino.de.montana@googlemail.com>
2012-07-23 15:37:31 +0100

    Bug fixes for websocket integration

--- static/javascript/download_worker.js
+++ static/javascript/download_worker.js
@@ -24,15 +24,41 @@ if (location.protocol == "http:") {
     var socket_protocol = "wss:";
 }
 var sock = new WebSocket(socket_protocol + "//" + location.host + "/workersocket");
-
+var SOCKOPEN = false;
+sock.onmessage = onSockMessage;
+sock.onopen = onSockOpen;
+sock.onclose = onSockClose;
+sock.onerror = onSockError;
 
 self.onmessage = function (event) {
     var msg = JSON.parse(event.data);
-    self.postMessage("Worker received command: " + msg.command);
+    parentSend("Worker received command: " + msg.command);
     if (msg.command == "start_download") {
         var t_id = msg.content.torrent_id;
-        self.postMessage("Worker got torrent_id: " + t_id);
-        sock.send(torrent_id);
+        parentSend("Worker got torrent_id: " + t_id);
+        if (SOCKOPEN) {
+            sockSend(t_id);
+        } else {
+            setTimeout(sockSend, 1000, t_id);
+        }
     }
 }
- 
+
+function parentSend(message) {
+    self.postMessage(message);
+}
+function sockSend(message) {
+    sock.send(message);
+}
+function onSockMessage(event) {
+    
+}
+function onSockOpen(event) {
+    SOCKOPEN = true;
+}
+function onSockClose(event) {
+    SOCKOPEN = false;
+}
+function onSockError(event) {
+    
+}
