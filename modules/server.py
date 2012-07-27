ad81875519337a33ac8a76bcd24ba12b89bea5e1
mountainpenguin <pinguino.de.montana@googlemail.com>
2012-07-27 16:28:27 +0100

    Issue #33
    New module 'downloadHandler':
    allows assignment and storage of authentication tokens to file paths
    
    Save authentication token in memory via new module 'downloadHandler'
    
    Initialise new module 'downloadHandler' in main thread to allow sharing
    between modules

--- modules/server.py
+++ modules/server.py
@@ -30,6 +30,7 @@ from modules import create
 from modules import rss
 from modules import torrentHandler
 from modules import aliases
+from modules import downloadHandler
 from modules.Cheetah.Template import Template
 
 from modules import ajaxPage
@@ -843,7 +844,8 @@ class Main(object):
         application._pyrtRT = rtorrent.rtorrent(c.get("rtorrent_socket"))    
         application._pyrtL = login.Login(conf=c, log=application._pyrtLog)
         application._pyrtAliasStorage = aliases.AliasStore(application._pyrtLog, application._pyrtRT)
-        application._pyrtAJAX = ajaxPage.Ajax(conf=c, RT=application._pyrtRT, Log=application._pyrtLog, Sockets=application._pyrtSockets, Aliases=application._pyrtAliasStorage)
+        application._pyrtDownloadHandler = downloadHandler.downloadHandler(application._pyrtLog)
+        application._pyrtAJAX = ajaxPage.Ajax(conf=c, RT=application._pyrtRT, Log=application._pyrtLog, Sockets=application._pyrtSockets, Aliases=application._pyrtAliasStorage, DLHandler=application._pyrtDownloadHandler)
         application._pyrtOPTIONS = optionsPage.Options(conf=c, RT=application._pyrtRT, aliases=application._pyrtAliasStorage)
         application._pyrtSTATS = statsPage.Index(conf=c, RT=application._pyrtRT, aliases=application._pyrtAliasStorage)
         application._pyrtRemoteStorage = remotes.RemoteStorage(log=application._pyrtLog)
