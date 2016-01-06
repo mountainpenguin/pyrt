08f3b5e7fe9c513fb91f38a267132d45b9c8bc35
mountainpenguin <pinguino.de.montana@googlemail.com>
2016-01-06 10:36:10 +0000

    Style fixes for login.py

--- modules/login.py
+++ modules/login.py
@@ -2,7 +2,7 @@
 
 """ Copyright (C) 2012 mountainpenguin (pinguino.de.montana@googlemail.com)
     <http://github.com/mountainpenguin/pyrt>
-    
+
     This file is part of pyRT.
 
     pyRT is free software: you can redistribute it and/or modify
@@ -19,8 +19,6 @@
     along with pyRT.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import cookielib
-import Cookie
 import cPickle as pickle
 import os
 import hashlib
@@ -33,36 +31,37 @@ import math
 import logging
 import traceback
 
+
 class User(object):
     def __init__(self, pass_hash, sess_id=None, testing=[]):
         self.password = pass_hash
         self.sess_id = sess_id
         self.testing = testing
-        
+
+
 class Login:
     def __init__(self, conf=config.Config(), log=None):
         self.C = conf
         self.Log = log
-        #get this from a pickled object
-        #get pyrt root dir
+        # get this from a pickled object
+        # get pyrt root dir
         try:
             self.USER = pickle.load(open(".user.pickle"))
         except:
-            #self.USER = User("mountainpenguin", self.hashPassword("testing"))
             self.USER = User(self.C.CONFIG.password)
         self.PERM_SALT = self.USER.password.split("$")[1]
-        
+
     def _flush(self):
         pickle.dump(self.USER, open(".user.pickle", "w"))
-        
+
     def _getTimeToken(self):
-        return "%i" % math.floor( time.time() / 120 )
-        
+        return "%i" % math.floor(time.time() / 120)
+
     def getRPCAuth(self):
-        if self.USER.__dict__.has_key("rpcauth"):
+        if hasattr(self.USER, "rpcauth"):
             return self.USER.rpcauth
         else:
-            #gen random
+            # gen random
             rand = hashlib.sha256(os.urandom(20)).hexdigest()
             self.USER.rpcauth = rand
             return rand
@@ -89,7 +88,7 @@ class Login:
         return self.USER.password.split("$")[1]
 
     def checkPassword(self, pw, ip):
-        #pw = TOTP hash
+        # pw = TOTP hash
         try:
             TOTP_salt = pw.split("$")[1]
         except IndexError:
@@ -99,9 +98,9 @@ class Login:
         except:
             logging.error("Unhandled error in login, traceback: %s", traceback.format_exc())
             return False
-        
+
         hashed = self.USER.password
-        perm_salt = hashed.split("$")[1]
+#        perm_salt = hashed.split("$")[1]
         pwhash = hashed.split("$")[2]
         currToken = self._getTimeToken()
         token_salt = hashlib.sha256(currToken + TOTP_salt).hexdigest()
@@ -113,7 +112,7 @@ class Login:
         else:
             self.Log.warning("LOGIN: Attempted login from %s failed (invalid password)", ip)
             return False
-                
+
     def checkLogin(self, cookies, ipaddr):
         try:
             session_id = cookies.get("sess_id").value
@@ -128,17 +127,17 @@ class Login:
                 self.Log.debug("Session for %s has expired", ipaddr)
                 return False
 
-            #for sess_id in self.USER.testing:
-            #    h1 = hashlib.sha256(sess_id).hexdigest()
-            #    h2 = hashlib.sha256(h1 + ipaddr).hexdigest()
-            #    h3 = hashlib.sha256(h2 + salt).hexdigest()
-            #    if "$%s$%s" % (salt, h3) == session_id:
-            #        return True
-            #self.Log.debug("Session for %s.*.*.* has expired" % ipaddr.split(".")[0])
-            #return False
+#            for sess_id in self.USER.testing:
+#                h1 = hashlib.sha256(sess_id).hexdigest()
+#                h2 = hashlib.sha256(h1 + ipaddr).hexdigest()
+#                h3 = hashlib.sha256(h2 + salt).hexdigest()
+#                if "$%s$%s" % (salt, h3) == session_id:
+#                    return True
+#            self.Log.debug("Session for %s.*.*.* has expired" % ipaddr.split(".")[0])
+#            return False
         except:
             return False
-        
+
     def hashPassword(self, pw, salt=None):
         if not salt:
             salt = os.urandom(6)
@@ -146,7 +145,7 @@ class Login:
         hash_1 = hashlib.sha256(pw).hexdigest()
         hash_2 = hashlib.sha256(hash_1 + salt_encoded).hexdigest()
         return "$%s$%s" % (salt_encoded, hash_2)
-        
+
     def loginHTML(self, msg=""):
         return """
         <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
@@ -170,16 +169,16 @@ class Login:
                 </div>
             </body>
         </html>
-        """ % { "PERM_SALT" : self.PERM_SALT, "msg" : msg }
-        
+        """ % {"PERM_SALT": self.PERM_SALT, "msg": msg}
+
     def sendCookie(self, ipaddr):
         randstring = "".join([random.choice(string.letters + string.digits) for i in range(20)])
-        #add sess_id to self.USER
+        # add sess_id to self.USER
         self.USER.sess_id = randstring
-        #self.USER.testing += [randstring]
-        self._flush()  
+#        self.USER.testing += [randstring]
+        self._flush()
 
-        #hash sess_id
+        # hash sess_id
         h1 = hashlib.sha256(randstring).hexdigest()
         h2 = hashlib.sha256(h1 + ipaddr).hexdigest()
         rand_salt = base64.b64encode(os.urandom(10))
